2b739cfecf34d5669537d226655d52b06a55c777
Mychael M <mychaelbmenges@gmail.com>
2020-06-24 19:40:35 -0600

    created function to create a new menu item object

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+    let newMenuItem = {name: name, cost: cost, category: category};
+    return newMenuItem;
 }
+
+createMenuItem("Spaghetti", 12.5, "Dinner");
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
